ef7e2bdb0a3b4b3c1979a86abc6e63bba135f19f
somzzz <lucia.mcojocaru@gmail.com>
2017-01-16 04:03:06 -0800

    wip

--- src/object.d
+++ src/object.d
@@ -3240,51 +3240,8 @@ template RTInfo(T)
 {
     enum RTInfo = null;
 }
-/*
-import core.stdc.stdint;
-void enforceRawArraysConformable(const char[] action, in size_t elementSize,
-    const void[] a1, const void[] a2, in bool allowOverlap = false)
-{
-    _enforceSameLength(action, a1.length, a2.length);
-    if(!allowOverlap)
-        _enforceNoOverlap(action, arrayToPtr(a1), arrayToPtr(a2), elementSize * a1.length);
-}
 
-private void _enforceSameLength(const char[] action,
-    in size_t length1, in size_t length2)
-{
-    if(length1 == length2)
-        return;
-
-    UnsignedStringBuf tmpBuff = void;
-    string msg = "Array lengths don't match for ";
-    msg ~= action;
-    msg ~= ": ";
-    msg ~= length1.unsignedToTempString(tmpBuff, 10);
-    msg ~= " != ";
-    msg ~= length2.unsignedToTempString(tmpBuff, 10);
-    throw new Error(msg);
-}
-
-private void _enforceNoOverlap(const char[] action,
-    uintptr_t ptr1, uintptr_t ptr2, in size_t bytes)
-{
-    const d = ptr1 > ptr2 ? ptr1 - ptr2 : ptr2 - ptr1;
-    if(d >= bytes)
-        return;
-    const overlappedBytes = bytes - d;
-
-    UnsignedStringBuf tmpBuff = void;
-    string msg = "Overlapping arrays in ";
-    msg ~= action;
-    msg ~= ": ";
-    msg ~= overlappedBytes.unsignedToTempString(tmpBuff, 10);
-    msg ~= " byte(s) overlap of ";
-    msg ~= bytes.unsignedToTempString(tmpBuff, 10);
-    throw new Error(msg);
-}*/
-
-ref D _d_arraycopyT(S, D)(S from, D to)
+ref D _d_arraycopyT(S, D)(D to, S from)
 {
     import core.stdc.string;
 
@@ -3294,7 +3251,7 @@ ref D _d_arraycopyT(S, D)(S from, D to)
 
 
     (() @trusted => memcpy(cast(void*)to.ptr, from.ptr, to.length * size))();
-    return *to;
+    return to;
 }
 
 
